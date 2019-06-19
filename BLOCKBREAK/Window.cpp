5ef297ef32c1cc36a5768c6b29e64fe8c6ce5ec1
MinoriNakamura <saintlyterritory526@gmail.com>
2019-06-19 11:53:42 +0900

    Window.cppにWindow生成、WndProcを作成。 Window.hにWndProcのプロトタイプ宣言を作成。

--- BLOCKBREAK/Window.cpp
+++ BLOCKBREAK/Window.cpp
@@ -2,11 +2,39 @@
 #include <Windows.h>
 #include <d3dx9.h>
 
-VOID WindowGeneration::OutputWindow(WNDCLASSEX WndClass)
+VOID WindowGeneration::OutputWindow(WNDCLASSEX WndClass, HINSTANCE hInst)
 {
 	static char szAppName[] = "BROCKBREAK";
 
 	WndClass.cbSize = sizeof(WndClass);
-	WndClass.
-}
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hInstance = hInst;
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	WndClass.lpszMenuName = NULL;
+	WndClass.lpszClassName = szAppName;
+	WndClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
 
+}
+LRESULT CALLBACK WindowGeneration::WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (iMsg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	case WM_KEYDOWN:
+		switch ((CHAR)wParam)
+		{
+		case VK_ESCAPE:
+			PostQuitMessage(0);
+			break;
+		}
+		break;
+	}
+	return DefWindowProc(hWnd, iMsg, wParam, lParam);
+}
